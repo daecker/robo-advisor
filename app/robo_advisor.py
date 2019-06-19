7715d668a9d8f3dfb1cbc00d13ccd0a3ced975ca
daecker <50798284+daecker@users.noreply.github.com>
2019-06-19 16:39:40 -0400

    Define and Print Latest Close Price

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -8,7 +8,7 @@ import json
 
 #INFO INPUTS
 
-request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo"
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 
 response = requests.get(request_url)
 print(type(response))
@@ -18,18 +18,22 @@ print(response.text)
 parsed_response = json.loads(response.text) #convert response variable from a string to a dictionary
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+latest_close = parsed_response["Time Series (Daily)"]["2019-02-20"]["4. close"]
 
+
+def to_usd(my_price):
+    return "${0:.2f}".format(my_price)
 
 #INFO OUTPUTS
 
 #print("-------------------------")
 #print("SELECTED SYMBOL: XYZ")
 #print("-------------------------")
-#print("REQUESTING STOCK MARKET DATA...")
+#print("REQUESTING STOCK MARKET DATA...") #look at shopping cart
 #print("REQUEST AT: 2018-02-20 02:00pm")
 #print("-------------------------")
 print("LATEST DAY: " + last_refreshed)
-#print("LATEST CLOSE: $100,000.00")
+print("LATEST CLOSE: " + to_usd(float(latest_close)))
 #print("RECENT HIGH: $101,000.00")
 #print("RECENT LOW: $99,000.00")
 #print("-------------------------")
