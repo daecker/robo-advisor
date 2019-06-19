baf0ef3275b3e46085a9edd10962da6295896b4d
daecker <50798284+daecker@users.noreply.github.com>
2019-06-19 16:27:33 -0400

    Print Last Refresh

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -17,21 +17,24 @@ print(response.text)
 
 parsed_response = json.loads(response.text) #convert response variable from a string to a dictionary
 
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+
 #INFO OUTPUTS
 
-print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
-print("-------------------------")
-print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
-print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
-print("RECENT HIGH: $101,000.00")
-print("RECENT LOW: $99,000.00")
-print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
-print("-------------------------")
-print("HAPPY INVESTING!")
-print("-------------------------")
+#print("-------------------------")
+#print("SELECTED SYMBOL: XYZ")
+#print("-------------------------")
+#print("REQUESTING STOCK MARKET DATA...")
+#print("REQUEST AT: 2018-02-20 02:00pm")
+#print("-------------------------")
+print("LATEST DAY: " + last_refreshed)
+#print("LATEST CLOSE: $100,000.00")
+#print("RECENT HIGH: $101,000.00")
+#print("RECENT LOW: $99,000.00")
+#print("-------------------------")
+#print("RECOMMENDATION: BUY!")
+#print("RECOMMENDATION REASON: TODO")
+#print("-------------------------")
+#print("HAPPY INVESTING!")
+#print("-------------------------")
